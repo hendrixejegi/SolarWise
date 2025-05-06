25f393dbcf570f4ec83629d63856654fe5cdf177
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-05-06 20:32:12 +0100

    c

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "@components/header";
 import Footer from "./components/footer";
 
 function App() {
+  const pathName = useLocation().pathname;
+
+  console.log(pathName);
   return (
     <>
       <Header />
